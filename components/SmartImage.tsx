004c465b30c9b2164750643a3c3e3b774c58c49b
Danonyeka <danonyeka@gmail.com>
2025-09-26 13:34:18 +0100

    Update SmartImage.tsx

--- components/SmartImage.tsx
+++ components/SmartImage.tsx
@@ -1,25 +1,19 @@
-'use client';
-import Image from 'next/image';
+import Image, { ImageProps } from 'next/image';
+import clsx from 'clsx';
 
-type Props = {
-  src: string;
+type Props = Omit<ImageProps, 'alt'> & {
   alt: string;
-  fill?: boolean;
   className?: string;
-  width?: number;
-  height?: number;
 };
 
-export default function SmartImage({ src, alt, fill, className, width, height }: Props) {
-  const isSvg = src?.toLowerCase().endsWith('.svg');
-  if (isSvg) {
-    if (fill) {
-      return <img src={src} alt={alt} className={`absolute inset-0 w-full h-full object-contain ${className || ''}`} />;
-    }
-    return <img src={src} alt={alt} className={className} width={width} height={height} />;
-  }
-  if (fill) {
-    return <Image src={src} alt={alt} fill className={className} />;
-  }
-  return <Image src={src} alt={alt} width={width || 800} height={height || 800} className={className} />;
+export default function SmartImage({ className, ...props }: Props) {
+  // Defaults that are safe: lazy below-the-fold, WebP/AVIF via next/image
+  return (
+    <Image
+      {...props}
+      className={clsx(className)}
+      // if you always pass width/height or use static import, CLS is avoided
+      // Next will auto-lazy for non-priority images
+    />
+  );
 }
