9c96af0625b7feb86776477f3ef89ed4b0826296
Danonyeka <danonyeka@gmail.com>
2025-09-26 18:40:45 +0100

    Update SmartImage.tsx

--- components/SmartImage.tsx
+++ components/SmartImage.tsx
@@ -1,19 +1,11 @@
-import Image, { ImageProps } from 'next/image';
-import clsx from 'clsx';
+import Image, { type ImageProps } from 'next/image';
 
 type Props = Omit<ImageProps, 'alt'> & {
   alt: string;
   className?: string;
 };
 
-export default function SmartImage({ className, ...props }: Props) {
-  // Defaults that are safe: lazy below-the-fold, WebP/AVIF via next/image
-  return (
-    <Image
-      {...props}
-      className={clsx(className)}
-      // if you always pass width/height or use static import, CLS is avoided
-      // Next will auto-lazy for non-priority images
-    />
-  );
+export default function SmartImage({ className = '', ...props }: Props) {
+  // Keep it simple: pass className straight through, no clsx needed
+  return <Image {...props} className={className} />;
 }
