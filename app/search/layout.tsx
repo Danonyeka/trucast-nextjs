26ae4ab1ce4ff4ec2766dcb3c7c3a44e76406131
Danonyeka <danonyeka@gmail.com>
2025-09-29 22:26:07 +0100

    Create layout.tsx

---
+++ app/search/layout.tsx
@@ -0,0 +1,14 @@
+// app/search/layout.tsx
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  robots: { index: false, follow: true },
+}
+
+export default function SearchLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return <>{children}</>
+}
