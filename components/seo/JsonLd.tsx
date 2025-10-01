9b913b6b554995f74ecc3ac3691235645a9152bf
Danonyeka <danonyeka@gmail.com>
2025-10-01 01:59:00 +0100

    Update JsonLd.tsx

--- components/seo/JsonLd.tsx
+++ components/seo/JsonLd.tsx
@@ -10,7 +10,6 @@ export default function JsonLd({ id, data }: { id?: string; data: Json }) {
     <script
       id={id}
       type="application/ld+json"
-      // eslint-disable-next-line react/no-danger
       dangerouslySetInnerHTML={{ __html: JSON.stringify(data) }}
     />
   )
